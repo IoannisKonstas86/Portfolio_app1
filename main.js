21474d035b240684b68e9823bcb1883aa2f08f64
unforgiven <unforgiven_19@msn.com>
2021-10-03 21:00:56 +0200

    asdf

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 
-var genre = document.getElementById("genre").value;
-var titel = document.getElementById("titel").value;
-var year = document.getElementById("year").value;
+var movies = [];
 
-class movie{
+
+class Movie{
     constructor(genre,titel,year){
         this.genre = genre;
         this.titel = titel;
         this.year = year;
 
     }
-
 }
+    function clear(){
+        document.getElementById("myForm").reset();
+    }
 
+    function addMovie(){
+    let movie = new Movie(
+    document.getElementById("genre").value,
+    document.getElementById("titel").value,
+    document.getElementById("year").value
+    );
+    movies.push(movie);
+    clear();
+    
+}
+function printOut(data){
+    var table = document.getElementById("myTable")
 
-let movies = new movie();
-
+    data.forEach(element => {
+        var row = `<tr>
+                            <td>${element.genre}</td>
+                            <td>${element.titel}</td>
+                            <td>${element.year}</td>
+                    </tr>`
+            return table.innerHTML += row
+    }); 
+        
+    }
